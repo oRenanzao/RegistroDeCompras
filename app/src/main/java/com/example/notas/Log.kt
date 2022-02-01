f62bcf375f86417510f5ec60ef0a968ad2026b95
oRenanzao <renaedva@outlook.com>
2022-01-31 23:14:53 -0300

    Avanços: - Melhoria no layout - Possibilidade de adicionar diversos registros - Cógido para evitar erros adicionado - Adição de um botão para a página de histórico - Adição de um botão para limpar o histórico - Todos os dados agora estão armazenados em Shared Preferences
    Modificações to be adressed:
    - Funcionamento do botão de voltar
    - Possibilidade de edição dos registros
    - Possibilidade de excluir registros específicos
    - Verificar a possibilidade de colocar o botão de histórico na parte inferior da tela para melhor organização
    - Aumentar a quantidade de registros possíveis (atualmente são somente 3)

--- app/src/main/java/com/example/notas/Log.kt
+++ app/src/main/java/com/example/notas/Log.kt
@@ -1,25 +1,260 @@
 package com.example.notas
 
 import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AlertDialog
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.DividerItemDecoration
+import java.io.BufferedReader
 
 class Log : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log)
 
+        //Alterando o nome e o ícone da Action Bar
+        supportActionBar!!.setTitle("Histórico")
+        supportActionBar!!.setHomeAsUpIndicator(R.drawable.ic_seta2)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
         //Recebendo as variáveis de Main Activity
         val nometxt = intent.getStringExtra("nometxt")
+        val valortxt = intent.getStringExtra("valortxt")
+        val produtotxt = intent.getStringExtra("produtotxt")
+        val datatxt = intent.getStringExtra("datatxt")
+        val descricaotxt = intent.getStringExtra("descricaotxt")
+        val CONTTT = intent.getStringExtra("CONTTTtxt")
 
-        //Recuperando os objetos
-        val resultado = findViewById<TextView>(R.id.resultado)
+        //Recuperando os objetos para cada parte do layout
+        val btnLimpeza = findViewById<Button>(R.id.btnLimpeza)
 
-        //Exibindo os valores nas caixas
-        resultado.text = "Loja: $nometxt"
+        val lognome = findViewById<TextView>(R.id.lognome)
+        val logvalor = findViewById<TextView>(R.id.logvalor)
+        val logproduto = findViewById<TextView>(R.id.logproduto)
+        val logdata = findViewById<TextView>(R.id.logdata)
+        val logdescricao = findViewById<TextView>(R.id.logdescricao)
+
+        val lognome2 = findViewById<TextView>(R.id.lognome2)
+        val logvalor2 = findViewById<TextView>(R.id.logvalor2)
+        val logproduto2 = findViewById<TextView>(R.id.logproduto2)
+        val logdata2 = findViewById<TextView>(R.id.logdata2)
+        val logdescricao2 = findViewById<TextView>(R.id.logdescricao2)
+        val imgcalendar2 = findViewById<ImageView>(R.id.imgcalendar2)
+        val imgmoney2 = findViewById<ImageView>(R.id.imgmoney2)
+        val imgdesc2 = findViewById<ImageView>(R.id.imgdesc2)
+        val divider2 = findViewById<View>(R.id.divider2)
+
+        val lognome3 = findViewById<TextView>(R.id.lognome3)
+        val logvalor3 = findViewById<TextView>(R.id.logvalor3)
+        val logproduto3 = findViewById<TextView>(R.id.logproduto3)
+        val logdata3 = findViewById<TextView>(R.id.logdata3)
+        val logdescricao3 = findViewById<TextView>(R.id.logdescricao3)
+        val imgcalendar3 = findViewById<ImageView>(R.id.imgcalendar3)
+        val imgmoney3 = findViewById<ImageView>(R.id.imgmoney3)
+        val imgdesc3 = findViewById<ImageView>(R.id.imgdesc3)
+        val divider3 = findViewById<View>(R.id.divider3)
+
+        //Caixa de contagem
+        val tvteste2 = findViewById<TextView>(R.id.tvteste2)
+
+        tvteste2.text = "$CONTTT"
+
+        fun limparHistorico(){
+            val intent = Intent(this, MainActivity::class.java)
+
+            //Enviando um zero para a contagem na Main Activity
+            intent.putExtra("logcont", "Clicado")
+
+            //Voltando para a página Main
+            startActivity(intent)
+        }
+
+        //Escrevendo os valores nas caixas do layout 1
+        fun placeCont1(){
+            lognome.text = "$nometxt:"
+            logvalor.text = "$valortxt R$"
+            logproduto.text = "$produtotxt"
+            logdata.text = "$datatxt"
+            logdescricao.text = "$descricaotxt"
+        }
+
+        //Armazenando os valores que estavam nas caixas do layout 1
+        fun saveCont1() {
+            val sharedPreferences = getSharedPreferences("sharedPrefs", Context.MODE_PRIVATE)
+            val editor = sharedPreferences.edit()
+            editor.apply {
+                putString("NOME1", nometxt)
+                putString("VALOR1", valortxt)
+                putString("PRODUTO1", produtotxt)
+                putString("DATA1", datatxt)
+                putString("DESCRICAO1", descricaotxt)
+            }.apply()
+        }
+
+        //Retornando os valores que estavam nas caixas do layout 1
+        fun getCont1(){
+            val sharedPreferences = getSharedPreferences("sharedPrefs", Context.MODE_PRIVATE)
+            val nome1 = sharedPreferences.getString("NOME1", null)
+            val valor1 = sharedPreferences.getString("VALOR1", null)
+            val produto1 = sharedPreferences.getString("PRODUTO1", null)
+            val data1 = sharedPreferences.getString("DATA1", null)
+            val descricao1 = sharedPreferences.getString("DESCRICAO1", null)
+
+            //Escrevendo os valores novamente nas caixas
+            lognome.text = "$nome1:"
+            logvalor.text = "$valor1 R$"
+            logproduto.text = "$produto1"
+            logdata.text = "$data1"
+            logdescricao.text = "$descricao1"
+        }
+
+        //Escrevendo os valores nas caixas do layout 2
+        fun placeCont2(){
+
+            //Colocando os valores das caixas de layout 1 de volta
+            getCont1()
+
+            //Colocando os valores das caixas de layout 2
+            lognome2.text = "$nometxt:"
+            logvalor2.text = "$valortxt R$"
+            logproduto2.text = "$produtotxt"
+            logdata2.text = "$datatxt"
+            logdescricao2.text = "$descricaotxt"
+        }
+
+        //Armazenando os valores que estavam nas caixas do layout 2
+        fun saveCont2() {
+            val sharedPreferences = getSharedPreferences("sharedPrefs", Context.MODE_PRIVATE)
+            val editor = sharedPreferences.edit()
+            editor.apply {
+                putString("NOME2", nometxt)
+                putString("VALOR2", valortxt)
+                putString("PRODUTO2", produtotxt)
+                putString("DATA2", datatxt)
+                putString("DESCRICAO2", descricaotxt)
+            }.apply()
+        }
+
+        //Retornando os valores que estavam nas caixas do layout 2
+        fun getCont2(){
+            val sharedPreferences = getSharedPreferences("sharedPrefs", Context.MODE_PRIVATE)
+            val nome2 = sharedPreferences.getString("NOME2", null)
+            val valor2 = sharedPreferences.getString("VALOR2", null)
+            val produto2 = sharedPreferences.getString("PRODUTO2", null)
+            val data2 = sharedPreferences.getString("DATA2", null)
+            val descricao2 = sharedPreferences.getString("DESCRICAO2", null)
+
+            //Escrevendo os valores novamente nas caixas
+            lognome2.text = "$nome2:"
+            logvalor2.text = "$valor2 R$"
+            logproduto2.text = "$produto2"
+            logdata2.text = "$data2"
+            logdescricao2.text = "$descricao2"
+        }
+
+        fun placeCont3(){
+
+            //Colocando os valores das caixas de layout 1 de volta
+            getCont2()
+
+            //Colocando os valores das caixas de layout 2
+            lognome3.text = "$nometxt:"
+            logvalor3.text = "$valortxt R$"
+            logproduto3.text = "$produtotxt"
+            logdata3.text = "$datatxt"
+            logdescricao3.text = "$descricaotxt"
+        }
+
+        //Armazenando os valores que estavam nas caixas do layout 2
+        fun saveCont3() {
+            val sharedPreferences = getSharedPreferences("sharedPrefs", Context.MODE_PRIVATE)
+            val editor = sharedPreferences.edit()
+            editor.apply {
+                putString("NOME3", nometxt)
+                putString("VALOR3", valortxt)
+                putString("PRODUTO3", produtotxt)
+                putString("DATA3", datatxt)
+                putString("DESCRICAO3", descricaotxt)
+            }.apply()
+        }
+
+        //Retornando os valores que estavam nas caixas do layout 2
+        fun getCont3(){
+            val sharedPreferences = getSharedPreferences("sharedPrefs", Context.MODE_PRIVATE)
+            val nome3 = sharedPreferences.getString("NOME3", null)
+            val valor3 = sharedPreferences.getString("VALOR3", null)
+            val produto3 = sharedPreferences.getString("PRODUTO3", null)
+            val data3 = sharedPreferences.getString("DATA3", null)
+            val descricao3 = sharedPreferences.getString("DESCRICAO3", null)
+
+            //Escrevendo os valores novamente nas caixas
+            lognome3.text = "$nome3:"
+            logvalor3.text = "$valor3 R$"
+            logproduto3.text = "$produto3"
+            logdata3.text = "$data3"
+            logdescricao3.text = "$descricao3"
+        }
+
+        //Exibindo os valores nas caixas de acordo com a contagem de salvamentos
+
+        if (CONTTT == "1"){
+            placeCont1()
+            saveCont1()
+
+            //Deixando o resto invisível
+            imgcalendar2.isVisible = false
+            imgdesc2.isVisible = false
+            imgmoney2.isVisible = false
+            imgcalendar3.isVisible = false
+            imgdesc3.isVisible = false
+            imgmoney3.isVisible = false
+            divider2.isVisible = false
+            divider3.isVisible = false
+
+        } else{
+            if (CONTTT == "2"){
+                getCont1()
+                placeCont2()
+                saveCont2()
+
+                //Deixando o resto invisível
+                imgcalendar3.isVisible = false
+                imgdesc3.isVisible = false
+                imgmoney3.isVisible = false
+                divider3.isVisible = false
+
+            } else {
+                if (CONTTT == "3") {
+                    getCont1()
+                    getCont2()
+                    placeCont3()
+                    saveCont3()
+                }
+            }
+        }
+
+        btnLimpeza.setOnClickListener{
+            //Confirmando a limpeza dos dados
+            val builder = AlertDialog.Builder(this)
+            builder.setMessage("Tem certeza que deseja limpar o histórico?")
+                .setCancelable(false)
+                .setPositiveButton("Sim") { dialog, id ->
+                    limparHistorico()
+                }
+                .setNegativeButton("Não") { dialog, id ->
+                    // Dismiss the dialog
+                    dialog.dismiss()
+                }
+            val alert = builder.create()
+            alert.show()
+        }
 
     }
 }
--- app/src/main/java/com/example/notas/MainActivity.kt
+++ app/src/main/java/com/example/notas/MainActivity.kt
@@ -7,6 +7,9 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import java.io.BufferedReader
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,10 +17,49 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         //Reconhecendo os objetos
+        val btnClearCount = findViewById<Button>(R.id.btnClear)
+
         val tvnome = findViewById<TextView>(R.id.tvnome)
         val tvvalor = findViewById<TextView>(R.id.tvvalor)
-        val btnLog = findViewById<Button>(R.id.btnLog)
+        val tvproduto = findViewById<TextView>(R.id.tvproduto)
+        val tvdata = findViewById<TextView>(R.id.tvdata)
+        val tvdescricao = findViewById<TextView>(R.id.tvdescricao)
+        val btnSave = findViewById<Button>(R.id.btnLog)
+        val btnHist = findViewById<Button>(R.id.btnHist)
         val tvresultado = findViewById<TextView>(R.id.tvresultado)
+        val tvcontagem = findViewById<TextView>(R.id.tvcontagem)
+
+        //Definindo a caixa de data para ser a data atual automaticamente
+        fun data_atual(){
+            val data = Calendar.getInstance().time
+            val formatador = SimpleDateFormat("dd/MM/yyyy")
+            val data_formatada = formatador.format(data)
+            tvdata.text = data_formatada
+        }
+
+        val SAVECOUNT = "Salvando Contagem"
+
+        //Chamando a contagem pela primeira vez: retorna zero
+        fun getSaveCount() : Int {
+            val sharedPreferences = getSharedPreferences("sharedPrefs", Context.MODE_PRIVATE)
+            return sharedPreferences.getInt(SAVECOUNT, 0)
+        }
+
+        //Adicionando a contagem ao Shared Preferences
+        fun setSaveCount(count:Int) {
+            val sharedPreferences = getSharedPreferences("sharedPrefs", Context.MODE_PRIVATE)
+            val editor = sharedPreferences.edit()
+            editor.apply {
+                putInt(SAVECOUNT, count)
+            }.apply()
+        }
+
+        //Limpando a contagem
+        fun clearSaveCount(){
+            setSaveCount(0)
+            val SaveCount = getSaveCount()
+            tvcontagem.text = SaveCount.toString()
+        }
 
         //Salvando os dados
         fun saveData(){
@@ -25,16 +67,26 @@ class MainActivity : AppCompatActivity() {
             //Passando o valor informado para a caixa
             tvresultado.text = tvnome.text.toString()
 
+            //Pegando a contagem atual e adicionando mais um a cada vez que o botão é pressionado
+            var SaveCount = getSaveCount()
+            SaveCount++
+            setSaveCount(SaveCount)
+            tvcontagem.text = SaveCount.toString()
+
             //Salvando o resultado na chave NOME
             val sharedPreferences = getSharedPreferences("sharedPrefs", Context.MODE_PRIVATE)
             val editor = sharedPreferences.edit()
             editor.apply{
                 putString("NOME", tvnome.text.toString())
                 putString("VALOR",tvvalor.text.toString())
+                putString("PRODUTO",tvproduto.text.toString())
+                putString("DATA",tvdata.text.toString())
+                putString("DESCRICAO",tvdescricao.text.toString())
+                putString("CONTTT", tvcontagem.text.toString())
             }.apply()
 
             //Enviando uma mensagem de conclusão
-            Toast.makeText(this, "Data saved", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Dados salvos", Toast.LENGTH_SHORT).show()
         }
 
         //Abrindo uma nova tela
@@ -46,21 +98,100 @@ class MainActivity : AppCompatActivity() {
             val sharedPreferences = getSharedPreferences("sharedPrefs", Context.MODE_PRIVATE)
             val savedNome = sharedPreferences.getString("NOME", null)
             val savedValor = sharedPreferences.getString("VALOR", null)
+            val savedProduto = sharedPreferences.getString("PRODUTO", null)
+            val savedData = sharedPreferences.getString("DATA", null)
+            val savedDescricao = sharedPreferences.getString("DESCRICAO", null)
+            val savedCONTTT = sharedPreferences.getString("CONTTT", null)
 
             //Enviando os dados para o Log
             intent.putExtra("nometxt", "$savedNome")
             intent.putExtra("valortxt", "$savedValor")
+            intent.putExtra("produtotxt", "$savedProduto")
+            intent.putExtra("datatxt", "$savedData")
+            intent.putExtra("descricaotxt", "$savedDescricao")
+            intent.putExtra("CONTTTtxt", "$savedCONTTT")
 
             startActivity(intent)
         }
 
-        //Ativando as funções no botão
-        btnLog.setOnClickListener{
-            saveData()
-            goToLog()
+        //Verifica se as caixas foram preenchidas corretamente
+        fun checkBoxes(){
+            if(tvnome.text.toString() == ""){
+                Toast.makeText(this, "Informe a Loja", Toast.LENGTH_SHORT).show()
+            } else{
+                if(tvvalor.text.toString() == ""){
+                    Toast.makeText(this, "Informe o valor", Toast.LENGTH_SHORT).show()
+                } else{
+                    if(tvdata.text.toString() == ""){
+                        Toast.makeText(this, "Informe a data", Toast.LENGTH_SHORT).show()
+
+                    } else {
+
+                    }
+                }
+            }
+        }
+
+        //Limpa as caixas de texto para outros input
+        fun clearBoxes(){
+            tvnome.text = ""
+            tvproduto.text = ""
+            tvvalor.text = ""
+            tvdescricao.text =""
+        }
+
+        //Exibindo a contagem ao fechar e abrir o aplicativo
+        tvcontagem.text = getSaveCount().toString()
+
+        //Verificando se houve limpeza de histórico
+        if (intent.getStringExtra("logcont").toString() == "Clicado"){
+            tvcontagem.text = intent.getStringExtra("logcont").toString()
+            clearSaveCount()
+            }
+
+        //Colocando o valor da data automaticamente
+        data_atual()
+
+        //Botão para salvar os dados
+        btnSave.setOnClickListener{
+
+            //Verificando se as caixas foram preenchidas corretamente
+//            if(tvnome.text.toString() == ""){
+//                Toast.makeText(this, "Informe a Loja", Toast.LENGTH_SHORT).show()
+//            } else{
+//                if(tvproduto.text.toString() == ""){
+//                    Toast.makeText(this, "Informe o Produto", Toast.LENGTH_SHORT).show()
+//                } else{
+//                    if(tvvalor.text.toString() == ""){
+//                        Toast.makeText(this, "Informe o Valor", Toast.LENGTH_SHORT).show()
+//                    } else{
+//                        if(tvdata.text.toString() == ""){
+//                            Toast.makeText(this, "Informe a Data", Toast.LENGTH_SHORT).show()
+//                        } else {
+//                            //Salvando os dados e limpando as caixas
+                        saveData()
+                        goToLog()
+                        clearBoxes()
+//                        }
+//                    }
+//                }
+//            }
+        }
+
+        //Botão para recuperar os dados
+        btnHist.setOnClickListener{
+            if (tvcontagem.text.toString() != "0"){
+                goToLog()
+            }
+            else{
+                Toast.makeText(this, "Não há nada no histórico", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        btnClearCount.setOnClickListener{
+            clearSaveCount()
         }
 
     }
-
 
 }
